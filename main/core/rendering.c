927395d0050d5cb69a3e85244caa884c5548c938
electric-otter <krw122081@gmail.com>
2024-12-25 21:07:45 -0800

    Create rendering.c

---
+++ main/core/rendering.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <math.h>
+#include <errno.h>
+    char *buffer = NULL;
+    printf("%f", sqrt(16));
+    chunk = ftell (fd) / 2;
+    printf("%f", pow(4, 3));
+    for (i = 0; i <= 10; i = i + 2) {
+  printf("%d\n", i);
+
+printf("%p\n", ptr);
+}
+    if ((buffer = malloc (chunk * sizeof *buffer)) == NULL) {
+                perror("Error rendering, the virtual memory/physical memory might be corrupted.");
+        exit(EXIT_FAILURE);
+      
+        return 1;
+    }
